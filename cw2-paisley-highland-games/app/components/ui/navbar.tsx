582f0eeb0e0a7498b1620c003db7596ec7fd0d68
David <91611885+FEDavid@users.noreply.github.com>
2025-11-18 11:56:58 +0000

    Updated NavBar

--- cw2-paisley-highland-games/app/components/ui/navbar.tsx
+++ cw2-paisley-highland-games/app/components/ui/navbar.tsx
@@ -3,35 +3,45 @@
 import Link from "next/link";
 
 export default function Navbar() {
-  return (
-    <nav className="w-full  px-8 py-4 shadow-md bg-gray-100 dark:bg-gray-900 mb-2">
-        <ul className="flex space-x-6">
-            <li>
-                <Link href="/" className="text-gray-800 dark:text-gray-200 hover:underline">
-                    Home
-                </Link>
-            </li>
-            <li>
-                <Link href="/about" className="text-gray-800 dark:text-gray-200 hover:underline">
-                    About
-                </Link>
-            </li>
-            <li>
-                <Link href="/events" className="text-gray-800 dark:text-gray-200 hover:underline">
-                    Events
-                </Link>
-            </li>
-            <li>
-                <Link href="/contact" className="text-gray-800 dark:text-gray-200 hover:underline">
-                    Contact
-                </Link>
-            </li>
-            <li>
-                <Link href="/teams" className="text-gray-800 dark:text-gray-200 hover:underline">
-                    Teams
-                </Link>
-            </li>
-        </ul>
-    </nav>
-  );
+    return (
+        <nav className="w-full px-8 pt-4 pb-5 shadow-md bg-gray-100 dark:bg-gray-900 mb-2 flex justify-between items-center">
+            <div id="leftside_nav" className="flex flex-row gap-8">
+                <p className="font-black">Paisley Highland Games</p>
+                <ul className="flex space-x-6">
+                    <li>
+                        <Link href="/" className="text-gray-800 dark:text-gray-200 hover:underline">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/events" className="text-gray-800 dark:text-gray-200 hover:underline">
+                            Events
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/teams" className="text-gray-800 dark:text-gray-200 hover:underline">
+                            Teams
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact" className="text-gray-800 dark:text-gray-200 hover:underline">
+                            Contact
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className="text-gray-800 dark:text-gray-200 hover:underline">
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div id="rightside_nav" className="flex flex-row gap-8 items-center">
+                <div id="searchbar" className="w-48 bg-gray-300 rounded-4xl px-3 py-1 text-(--background)">Search</div>
+                <div id="profile_section" className="flex flex-row items-center gap-2 cursor-pointer bg-(--background) px-3 py-1 rounded-4xl hover:shadow-md">
+                    <p className="px-2">Login</p>
+                    <div id="profile_icon" className="w-8 h-8 bg-gray-400 rounded-full inline-block"></div>
+                </div>
+            </div>
+        </nav>
+    );
 }
